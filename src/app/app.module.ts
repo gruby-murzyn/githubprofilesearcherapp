5b77d340b1ec96e27199fbfc7b5816707622676b
gruby-murzyn <jellyforjully088@yahoo.com>
2018-06-07 19:39:24 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GithubService } from './github.service';
+import {  HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [GithubService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
+import 'rxjs/add/operator/map';
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user [];
+  repos [];
+  username: string;
+  constructor(private _githubService: GithubService) {
+      this.user = false;
+  }
+
+  searchUser() {
+     this._githubService.updateUser(this.username);
+
+     this._githubService.getUser().subscribe(user => {
+          // console.log(user);
+          this.user = user;
+      });
+
+      this._githubService.getRepos().subscribe(repos => {
+          this.repos = repos;
+      });
+  }
+  ngOnInit() {
+  }
+
+}
